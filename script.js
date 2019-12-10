7d5ea33502624561ae7716efa743d9b95575e015
Paul Kirschner <p.kirschner01@gmail.com>
2019-12-09 19:18:16 -0500

    created var list.  Think it's complete

---
+++ script.js
@@ -0,0 +1,13 @@
+//variable list
+var specChar = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\ ";
+var num = " 0123456789 ";
+var lowCase = " abcdefghijklmnopqrstuvwxyz ";
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+//more var
+var specCharInput = document.getElementById("specChar");
+var numInput = document.getElementById("num");
+var lowCaseInput = document.getElementById("lowCase");
+var upCaseInput = document.getElementById("upCase");
+var lengthInput = document.getElementById("lenght");
+var passwordField = document.getElementById("passwordField");
+var createButton = document.getElementById("create");
