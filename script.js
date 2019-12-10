84ed28288549ebb89fd7bec838f722c2561f0067
Paul Kirschner <p.kirschner01@gmail.com>
2019-12-09 22:25:37 -0500

    minor changes to js file. long way to go.

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ var upCaseInput = document.getElementById("upCase");
 var lengthInput = document.getElementById("lenght");
 var passwordField = document.getElementById("passwordField");
 var createButton = document.getElementById("create");
+var userPWord;
+var pwordGroup;
+var pwordlenght;
 
+//loop
+for (var x = 0; x < 128; x++) {
+
+//variables
+var userInput1 = pwordlength[Math.floor(Math.random() * pwordlength.length)];
+var pwordlength = prompt ("How many characters would you like your password to be?");
+//if
+if (userInput1 >=128 && userInput1 === typeof(number))    
+    alert("You chose " + userInput1)
 
